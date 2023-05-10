d980bd53c03f3ab128cf1f8bde8f52b64cb35013
muhiminul-islam <islammuhiminul11@gmail.com>
2023-05-11 02:46:41 +0600

    login sign up with firebase integration done

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -6,6 +6,8 @@ const NavBar = () => {
     const navItems = <>
     <li><Link to='/'>Home</Link></li>
     <li><Link to='/about'>About</Link></li>
+    <li><Link to='/login'>Login</Link></li>
+    <li><Link to='/signup'>Sign Up</Link></li>
     </>
 
     return (
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,47 @@
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    // const [loading, setLoadig] = useState(true)
+
+    const register = (email, password) => {
+        // setLoadig(true)
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const login = (email, password) => {
+        // setLoadig(true)
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('current user', currentUser);
+            // setLoadig(false);
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        // loading,
+        register,
+        login,
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
