1d9e2fe63b4262bebb7ea44bef823edb1b49f91d
muhiminul-islam <islammuhiminul11@gmail.com>
2023-05-13 22:33:45 +0600

    upto token validaiton and request token done

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,20 +1,35 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import BookingRow from "./BookingRow";
+import { useNavigate } from "react-router-dom";
 
 
 const Bookings = () => {
 
     const { user } = useContext(AuthContext);
     const [bookings, setBoookings] = useState([]);
+    const navigate = useNavigate();
 
     const url = `http://localhost:5000/bookings?email=${user?.email}`
 
     useEffect(() => {
-        fetch(url)
+        fetch(url, {
+            method: "GET",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('car-doctor-token')}`
+            }
+        })
             .then(res => res.json())
-            .then(data => setBoookings(data))
-    }, [])
+            .then(data => {
+                if (!data.error) {
+                    setBoookings(data)
+                }
+                else{
+                    // logout and then navigate
+                    navigate('/');
+                }
+            })
+    }, [url, navigate])
 
     const handleDelete = id => {
         const proceed = confirm('Are you sure you want to delete?')
@@ -28,7 +43,7 @@ const Bookings = () => {
                     if (data.deletedCount > 0) {
                         alert('deleted successfull');
                         const remaining = bookings.filter(booking => booking._id !== id);
-                        setBoookings(remaining); 
+                        setBoookings(remaining);
                     }
                 })
         }
@@ -37,23 +52,23 @@ const Bookings = () => {
     const handleBookingConfirm = id => {
         fetch(`http://localhost:5000/bookings/${id}`, {
             method: "PATCH",
-            headers:{
+            headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify({status: 'confirm'})
+            body: JSON.stringify({ status: 'confirm' })
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            if(data.modifiedCount > 0){
-                const remaining = bookings.filter(booking => booking._id !== id);
-                const updated = bookings.find(booking => booking._id === id);
-                updated.status = 'confirm';
-                const newBookings  =[updated, ...remaining];
-                setBoookings(newBookings);
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount > 0) {
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    const updated = bookings.find(booking => booking._id === id);
+                    updated.status = 'confirm';
+                    const newBookings = [updated, ...remaining];
+                    setBoookings(newBookings);
 
-            }
-        })
+                }
+            })
     }
 
     return (
